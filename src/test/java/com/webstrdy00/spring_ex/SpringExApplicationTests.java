b636af232027f52dbeaa62407a141efa193ab0e0
webstrdy00 <les_roka22@naver.com>
2024-08-09 13:56:10 +0900

    초기 세팅

---
+++ src/test/java/com/webstrdy00/spring_ex/SpringExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webstrdy00.spring_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
