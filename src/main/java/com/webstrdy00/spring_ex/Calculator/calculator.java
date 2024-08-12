d90f6608d1be6d80baf54c0eb5c765be868bfd60
webstrdy00 <les_roka22@naver.com>
2024-08-12 13:30:01 +0900

    테스트 코드 추가

---
+++ src/main/java/com/webstrdy00/spring_ex/Calculator/calculator.java
@@ -0,0 +1,22 @@
+package com.webstrdy00.spring_ex.Calculator;
+
+public class calculator {
+    public Double operate(double num1, String op, double num2) {
+        switch (op) {
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    return null;
+                }
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException("잘못된 연산자입니다.");
+        }
+    }
+}
---
+++ src/test/java/com/webstrdy00/spring_ex/Calculator/calculatorTest.java
@@ -0,0 +1,27 @@
+package com.webstrdy00.spring_ex.Calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+    @Test
+    @DisplayName("더하기 테스트")
+    void test1() {
+        calculator calculator = new calculator();
+        Double result = calculator.operate(8, "+", 2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    @DisplayName("나누기 테스트")
+    void test2() {
+        calculator calculator = new calculator();
+        Double result = calculator.operate(8, "/", 2);
+        System.out.println("result = " + result);
+
+        Assertions.assertEquals(4, result);
+    }
+}
